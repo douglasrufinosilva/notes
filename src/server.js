fe47aa8200ca88eabe1389ef82149b400eb16932
Douglas Rufino <douglasrufinosilva@gmail.com>
2024-01-12 11:35:35 -0300

    Primeiro commit

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express()
+app.use(express.json())
+
+app.post("/users", (request, response) => {
+  const { name, email, password } = request.body
+  
+  
+  response.json({ name, email, password })
+})
+
+const PORT = 3003
+
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
